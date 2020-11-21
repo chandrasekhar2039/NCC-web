7083b811888dc5c4ca62ca9239d6ca84bbcfac18
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-11-21 05:44:45 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: KnightOne - v2.1.0
-* Template URL: https://bootstrapmade.com/knight-simple-one-page-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
